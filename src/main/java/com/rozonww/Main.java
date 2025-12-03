2094d224dd7711a7faa5da423dfbf0692ab47d25
Arnold Rozon <arnold@rozon.org>
2025-12-02 20:42:39 -0400

    Refactored route

--- src/main/java/com/rozonww/Main.java
+++ src/main/java/com/rozonww/Main.java
@@ -14,19 +14,8 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Dotenv env = Dotenv
-                .configure()
-                .ignoreIfMissing()
-                .ignoreIfMalformed()
-                .load();
-        MailerConfiguration config = new MailerConfiguration(
-                env.get("SMTP_HOST"),
-                Integer.parseInt(env.get("SMTP_PORT")),
-                env.get("SMTP_USERNAME"),
-                env.get("SMTP_API_KEY"),
-                env.get("SMTP_SENDER")
-        );
-        MailerService mailer = new MailerService(config);
+        Dotenv env = getEnvironmentValues();
+        MailerService mailer = getMailerService(env);
 
         var app = Javalin.create()
                 .get("/", ctx -> ctx.result("Hola mundo"))
@@ -63,6 +52,26 @@ public class Main {
             ctx.status(400);
             ctx.json(Map.of("success", false, "message", errorMessage));
         });
+    }
+
+    public static Dotenv getEnvironmentValues() {
+        return Dotenv
+                .configure()
+                .ignoreIfMissing()
+                .ignoreIfMalformed()
+                .load();
+    }
+
+    public static MailerService getMailerService(Dotenv env) {
+        MailerConfiguration config = new MailerConfiguration(
+                env.get("SMTP_HOST"),
+                Integer.parseInt(env.get("SMTP_PORT")),
+                env.get("SMTP_USERNAME"),
+                env.get("SMTP_API_KEY"),
+                env.get("SMTP_SENDER")
+        );
+        
+        return new MailerService(config);
     }
 }
 
