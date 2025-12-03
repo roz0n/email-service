47923399374ca29ddb126a5f9367a85125f7bb6f
Arnold Rozon <arnold@rozon.org>
2025-12-02 20:35:15 -0400

    Added route validation

--- src/main/java/com/rozonww/Main.java
+++ src/main/java/com/rozonww/Main.java
@@ -2,15 +2,32 @@ package com.rozonww;
 
 import com.rozonww.models.EchoRequest;
 import com.rozonww.models.EchoResponse;
-import com.rozonww.services.mailer.MailerService;
 import com.rozonww.services.mailer.MailerConfiguration;
 import com.rozonww.services.mailer.MailerSendRequest;
+import com.rozonww.services.mailer.MailerService;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.Javalin;
+import io.javalin.validation.ValidationException;
+
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
+        Dotenv env = Dotenv
+                .configure()
+                .ignoreIfMissing()
+                .ignoreIfMalformed()
+                .load();
+        MailerConfiguration config = new MailerConfiguration(
+                env.get("SMTP_HOST"),
+                Integer.parseInt(env.get("SMTP_PORT")),
+                env.get("SMTP_USERNAME"),
+                env.get("SMTP_API_KEY"),
+                env.get("SMTP_SENDER")
+        );
+        MailerService mailer = new MailerService(config);
+
         var app = Javalin.create()
                 .get("/", ctx -> ctx.result("Hola mundo"))
                 .start(8080);
@@ -24,31 +41,27 @@ public class Main {
         });
 
         app.post("/send", ctx -> {
-            // Get request body
-            var req = ctx.bodyAsClass(MailerSendRequest.class);
+            MailerSendRequest req = ctx.bodyValidator(MailerSendRequest.class)
+                    .check(body -> body.recipientEmail != null && !body.recipientEmail.isBlank(), "Recipient email is required")
+                    .check(body -> body.subject != null && !body.subject.isBlank(), "Subject is required")
+                    .check(body -> body.body != null && !body.body.isBlank(), "Body is required")
+                    .get();
 
-            // TODO: Validate request body
-            // If valid, send the email using email service
-            // If not, return a JSON error
+            mailer.send(req.recipientEmail, req.subject, req.body);
+            ctx.json(Map.of("success", true));
+        });
 
-            // Create mailer and send email
-            Dotenv env = Dotenv
-                    .configure()
-                    .ignoreIfMissing()
-                    .ignoreIfMalformed()
-                    .load();
-            MailerConfiguration config = new MailerConfiguration(
-                    env.get("SMTP_HOST"),
-                    Integer.parseInt(env.get("SMTP_PORT")),
-                    env.get("SMTP_USERNAME"),
-                    env.get("SMTP_API_KEY"),
-                    env.get("SMTP_SENDER")
-            );
-            MailerService mailer = new MailerService(config);
+        app.exception(ValidationException.class, (e, ctx) -> {
+            var messages = e.getErrors()
+                    .values()
+                    .stream()
+                    .flatMap(list -> list.stream())
+                    .map(err -> err.getMessage())
+                    .toList();
+            var errorMessage = messages.getFirst();
 
-            mailer.send(req.recipientEmail,
-                    req.subject,
-                    req.body);
+            ctx.status(400);
+            ctx.json(Map.of("success", false, "message", errorMessage));
         });
     }
 }
